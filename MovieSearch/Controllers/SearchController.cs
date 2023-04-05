37aa397fee5152a6103a0fbca9b61f301fc66500
강민호 <65155257+peru0@users.noreply.github.com>
2023-04-05 23:08:43 +0900

    elastic 설정 추가

--- MovieSearch/Controllers/SearchController.cs
+++ MovieSearch/Controllers/SearchController.cs
@@ -1,9 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Elastic.Clients.Elasticsearch;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MovieSearch.Controllers;
 
 [ApiController]
 public class SearchController : ControllerBase
 {
+    private readonly ILogger<SearchController> _logger;
+    private readonly ElasticsearchClient _elasticsearch;
     
+    public SearchController(ILogger<SearchController> logger, ElasticsearchClient esClient)
+    {
+        _logger = logger;
+        _elasticsearch = esClient;
+    }
+    
+    [HttpGet("text")]
+    public async Task<ActionResult> SearchByText(string query)
+    {
+        // TODO
+        return Ok();
+    }
 }
--- MovieSearch/Program.cs
+++ MovieSearch/Program.cs
@@ -1,7 +1,23 @@
+using Elastic.Clients.Elasticsearch;
+using Elastic.Transport;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Elasticsearch 설정
+var elasticSettings =
+    new ElasticsearchClientSettings(new Uri(builder.Configuration.GetConnectionString("ElasticHost")!))
+        .ServerCertificateValidationCallback((_, _, _, _) => true)
+        .Authentication(
+            new BasicAuthentication(
+                builder.Configuration.GetConnectionString("ElasticUser")!,
+                builder.Configuration.GetConnectionString("ElasticPassword")!
+            )
+        );
+var elastic = new ElasticsearchClient(elasticSettings);
 
+// Add services to the container.
+builder.Services.AddSingleton(elastic);
+    
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
