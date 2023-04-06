690cd8687b60e3e892691e5b0521ad484178642e
강민호 <65155257+peru0@users.noreply.github.com>
2023-04-06 12:33:21 +0900

    DbContext가 inject 되는 시점을 Controller 생성시로 변경

--- MovieSearch/Controllers/SearchController.cs
+++ MovieSearch/Controllers/SearchController.cs
@@ -10,12 +10,14 @@ namespace MovieSearch.Controllers;
 public class SearchController : ControllerBase
 {
     private readonly ILogger<SearchController> _logger;
-    private readonly ElasticsearchClient _elasticsearch;
+    private readonly MovieDb _db;
+    private readonly ElasticsearchClient _elastic;
 
-    public SearchController(ILogger<SearchController> logger, ElasticsearchClient esClient)
+    public SearchController(ILogger<SearchController> logger, MovieDb db, ElasticsearchClient elastic)
     {
         _logger = logger;
-        _elasticsearch = esClient;
+        _db = db;
+        _elastic = elastic;
     }
 
     [HttpGet("text")]
@@ -26,32 +28,32 @@ public class SearchController : ControllerBase
     }
 
     [HttpPost("db_test")]
-    public async Task<ActionResult> TestAdd(MovieDb db, string name, string url)
+    public async Task<ActionResult> TestAdd(string name, string url)
     {
-        db.Infos.Add(new MovieInfo
+        _db.Infos.Add(new MovieInfo
         {
             Name = name,
             InfoUrl = url,
         });
-        await db.SaveChangesAsync();
+        await _db.SaveChangesAsync();
 
         return Ok();
     }
 
     [HttpGet("db_test_json")]
-    public async Task<ActionResult<List<MovieInfo>>> TestGet(MovieDb db, string name)
+    public async Task<ActionResult<List<MovieInfo>>> TestGet(string name)
     {
-        return await db.Infos
+        return await _db.Infos
             .Where(row => row.Name == name)
             .ToListAsync();
     }
 
     [HttpGet("db_test/{name}")]
-    public async Task<ActionResult<string?>> TestGetUrl(MovieDb db, string name)
+    public async Task<ActionResult<string?>> TestGetUrl(string name)
     {
         _logger.LogTrace("로깅 예제: Searching for {Name}...", name);
         
-        var url = await db.Infos
+        var url = await _db.Infos
             .Where(row => row.Name == name)
             .Select(row => row.InfoUrl)
             .FirstOrDefaultAsync();
